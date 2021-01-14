64e24e4c1e6a88a701d5b73171ec61b2e577df3c
papalucho1 <papelopez123@gmail.com>
2021-01-14 17:55:49 -0300

    login added
    se aggrego login en pagina de index.html  modelo persona

--- proshot/blog/models.py
+++ proshot/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from multiselectfield import MultiSelectField
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.text import slugify
     
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -19,6 +21,80 @@ class Post(models.Model):
         
     class Meta:
         ordering = ['created_on']
-
+        # acá me sale error 
         def __unicode__(self):
             return self.title
+
+LENGUAGES_PERSON = (
+            ('es', 'Español'),
+            ('en', 'Inglés'),
+            ('de', 'Alemán'),
+            ('fr', 'Francés'),
+            ('pt', 'Portugués'),
+            ('ru', 'Ruso'),
+)
+
+ESPECIALIDAD_CATEGORY =(
+    (1,'Matrimonio'),
+    (2,'Fiesta'),
+    (3,'Eventos Musicales'),
+    (4,'Eventos Deportivos'),
+    (5,'Mascota'),
+    (6,'Fotografía de producto'),
+    (7,'Fotografía aérea'),
+    )
+
+DISPONIBILIDAD_CATEGORY =(
+    (1, 'Full Time'),
+    (2, 'Part Time'),
+    (3, 'Casual'),
+    (4, 'Fin de Semanas'),
+    (8, 'Festivos'),
+    (5, 'Mañanas'),
+    (6, 'Tardes'),
+    (7, 'Noches'),
+
+)
+
+REGION_CHILE = [
+            ('XV', 'Arica y Parinacota'),
+            ('I', 'Tarapacá'),
+            ('II', 'Antofagasta'),
+            ('III', 'Atacama'),
+            ('IV', 'Coquimbo'),
+            ('V', 'Valparaíso'),
+            ('RM', 'Santiago'),
+            ('VI', 'Libertador General Bernardo Ohiggins'),
+            ('VII', 'Maule'),
+            ('XVI', 'Ñuble'),
+            ('VIII', 'Biobío'),
+            ('IX', 'La Araucanía'),
+            ('XIV', 'Los Ríos'),
+            ('X', 'Los lagos'),
+            ('XI', 'Aysen'),
+            ('XII', 'Magallanes y Antártica')
+    ]
+class Person(models.Model):
+    #nombre y apellidos
+    first_name = models.CharField(max_length=30 )
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    city = models.CharField(max_length=30)
+    #region 
+    region = models.CharField(max_length=40, choices=REGION_CHILE)
+    #instagram
+    instagram = models.CharField(max_length=50, null= True)
+    #url con el portafolio
+    portafolio = models.URLField()
+    #insertar 3 imagenes que representen al fotografo (opcional)
+    rutaImagen = models.FileField(null = True)
+    #idioma que domina el fotografo
+    idioma = MultiSelectField(choices=LENGUAGES_PERSON,  null=True)
+    #categoria de las cuales maneja en fotografia
+    categoriasEspecialidad = MultiSelectField(choices = ESPECIALIDAD_CATEGORY,  null=True)
+    #disponibilidad 
+    disponibilidad = MultiSelectField(choices = DISPONIBILIDAD_CATEGORY,  null=True)
+    #status se refiere a que si está dentro del sistema o no 
+    status = models.IntegerField()
+    
--- proshot/proshot/settings.py
+++ proshot/proshot/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog',
+    'multiselectfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
